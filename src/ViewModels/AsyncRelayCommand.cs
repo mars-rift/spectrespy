fdd6693c8f85c88e0ec49a6494375f6279a3450a
mars89 <mana.rift@outlook.com>
2025-05-13 03:40:04 -0600

    Added Async Relay

---
+++ src/ViewModels/AsyncRelayCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace spectrespy.ViewModels
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private readonly Func<bool>? _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<Task> execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => !_isExecuting && (_canExecute?.Invoke() ?? true);
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+            try { await _execute(); }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
